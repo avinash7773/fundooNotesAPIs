40c190bbc5c14f2c11196fbe105b97d629b4ae97
avinash7773 <javinash228@gmail.com>
2021-07-05 13:31:10 +0530

    Refactor : Files Name

--- config/logger.js
+++ config/logger.js
@@ -3,13 +3,13 @@ const {createLogger, transports,  format} = require("winston");
 const logger = createLogger({
     transports : [
         new transports.File({
-            filename : "./logFile/info",
+            filename : "./logFile/info.log",
             level : 'info',
             format : format.combine(format.timestamp(), format.json())
         }),
 
         new transports.File({
-           filename : "./logFile/error",
+           filename : "./logFile/error.log",
            level : "error",
            format : format.combine(format.timestamp(), format.json()) 
         })
